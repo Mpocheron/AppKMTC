d8190bd7cfb2bef498f39dc8564c522548941b74
Milowwa <145760117+Milowwa@users.noreply.github.com>
2024-01-16 12:13:42 +0100

    ajout de sécurité sur la modification des infos user

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -21,7 +21,18 @@ class UserController extends AbstractController
      */
     public function editUser(Request $request, User $user, EntityManagerInterface $entityManager, UserPasswordHasherInterface $userPasswordHasher): Response
     {
-        
+        //Si l'utilisateur n'est pas connecté, il est redirigé vers la page de connexion
+
+        if (!$this->getUser()) {
+            return $this->redirectToRoute('app_login');
+        }
+
+        //Ce code sert à vérifier que l'utilisateur connecté est bien le même que celui qui veut modifier son profil
+
+        if ($this->getUser() !== $user){
+            return $this->redirectToRoute('app_logout');
+        }
+
         $form = $this->createForm(EditUserType::class, $user);
         $form->handleRequest($request);
 
