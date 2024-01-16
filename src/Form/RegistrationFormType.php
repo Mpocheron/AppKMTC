30559112a0a18c4df4c0b1b3ffb1bec7b6c19d5f
Mpocheron <mateo.pocheron@gmail.com>
2024-01-16 12:31:04 +0100

    Ajout des champs user dans le form de création de compte

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -6,9 +6,11 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
+use Symfony\Component\Validator\Constraints\Regex;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 
@@ -20,7 +22,14 @@ class RegistrationFormType extends AbstractType
             ->add('email')
             ->add('nom')
             ->add('prenom')
-            ->add('telephone')
+            ->add('telephone', TelType::class, [
+                'constraints' => [
+                    new Regex([
+                        'pattern' => '/[0-9]/',
+                        'message' => 'Veuillez rentrer un numero de téléphone valide'
+                    ])
+                ]
+            ])
             ->add('agreeTerms', CheckboxType::class, [
                                 'mapped' => false,
                 'constraints' => [
@@ -39,11 +48,12 @@ class RegistrationFormType extends AbstractType
                         'message' => 'Please enter a password',
                     ]),
                     new Length([
-                        'min' => 6,
+                        'min' => 8,
                         'minMessage' => 'Your password should be at least {{ limit }} characters',
                         // max length allowed by Symfony for security reasons
                         'max' => 4096,
                     ]),
+                    new Regex([])
                 ],
             ])
         ;
