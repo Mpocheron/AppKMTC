3251c77eaec576c2003009e149ae03c290d4a231
Mpocheron <mateo.pocheron@gmail.com>
2024-03-25 19:04:19 +0100

    Suppression de lignes inutiles dans UserController

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,22 +4,19 @@ namespace App\Controller;
 
 use App\Entity\User; 
 use App\Form\UserType; 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController; 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response; 
-use Symfony\Component\Routing\Annotation\Route; 
+use Symfony\Component\Routing\Annotation\Route;
 
 class UserController extends AbstractController
 {
-
     #[Route('/user', name: 'app_user')] // Définit la route '/user' avec le nom 'app_user'
     /**
      * @Route("/user/edit", name="user_edit") // Définit la route '/user/edit' avec le nom 'user_edit'
      */
-    public function editUser(User $user): Response // Définit la méthode editUser qui prend un User en paramètre et retourne une Response
+    public function editUser(User $user, Request $request): Response // Définit la méthode editUser qui prend un User en paramètre et retourne une Response
     {
-
-        $request = $this->container->get('request_stack')->getCurrentRequest(); // Récupère la requête courante
-
         $form = $this->createForm(UserType::class, $user); // Crée le formulaire à partir du UserType et du User
         $form->handleRequest($request); // Gère la requête sur le formulaire
         if ($form->isSubmitted() && $form->isValid()) { // Si le formulaire est soumis et valide
@@ -28,15 +25,8 @@ class UserController extends AbstractController
         }
 
         return $this->render( // Rend la vue
-            'user/edit.html.twig', 
-            [
+            'user/edit.html.twig', [
                 'form' => $form->createView(), // Passe la vue du formulaire
-                'user' => $user, // Passe l'utilisateur
-                'controller_name' => 'UserController', // Passe le nom du controller
-
-            ]
-        );
-
+            ]);
     }
-
 }
